32f6d7ea26cfd0ff344f64905a151bdcba5eebaf
Bawantha <bawanthapasqual@gmail.com>
2024-10-16 09:41:14 +0530

    login

---
+++ src/store/api/authApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4002/" }),
+
+  reducerPath: "authApi",
+  endpoints: (build) => ({
+    loginUser: build.mutation({
+      query: (data) => {
+        return {
+          url: "user/login",
+          method: "POST",
+          body: data,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useLoginUserMutation } = authApi;
