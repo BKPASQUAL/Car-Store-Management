3670961ed56ba319d878523a1e0e82c126320ff3
Bawantha <bawanthapasqual@gmail.com>
2024-10-20 10:04:22 +0530

    change Base url

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -3,8 +3,8 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 // Initialize an empty api service that we'll inject endpoints into later as needed
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:4002/',
-        // baseUrl: 'http://44.208.32.102:3001/',
+        // baseUrl: 'http://localhost:4002/',
+        baseUrl: 'http://3.104.75.209:4002/',
 
         prepareHeaders: (headers) => {
             const accessToken = localStorage.getItem('accessToken'); 
